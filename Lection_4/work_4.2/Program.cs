48516dfa21c6fb0c581999a2fdd32d4db256f6fa
IrinaV <9028944374@mail.ru>
2022-10-21 21:25:03 +0500

    L4

---
+++ Lection_4/work_4.2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Определим двумерный массив, назовём его matrix и укажем для него,  3 строки и 4 столбца. 
+// i (внешний цикл, щёлкающий строки)
+// columns — j(внутренний цикл, щёлкающий столбцы). Выводим на экран. 
+
+//int[,] matrix = new int[3, 4];
+//for (int i = 0; i < 3; i++)
+//{
+// for (int j = 0; j < 4; j++)
+// {
+// Console.Write($"{matrix[i, j]} ");
+// }
+//Console.WriteLine();
+//}
+
+//int[,] matrix = new int[3, 4];
+//for (int i = 0; i < matrix.GetLength(0); i++)
+//{
+// for (int j = 0; j < matrix.GetLength(1); j++)
+// {
+// Console.Write($"{matrix[i, j]} ");
+// }
+//Console.WriteLine();
+//}
+
+
+void PrintArray(int[,] matr)
+{
+ for (int i = 0; i < matr.GetLength(0); i++)
+ {
+ for (int j = 0; j < matr.GetLength(1); j++)
+ {
+ Console.Write($"{matr[i, j]} ");
+ }
+ Console.WriteLine();
+ }
+}
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
